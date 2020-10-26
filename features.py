3f5934b1151f87c485f6fd9d45cfb89ddf972d8b
lakruone <lakruone@gmail.com>
2020-10-26 12:34:41 +0530

    add two features

--- features.py
+++ features.py
@@ -1,13 +1,24 @@
 import regex
+from tldextract import extract
+import ipaddress
+
 
 
 def main(url):
     print("url : " +url)
 
 
-    check = [[url_length(url),having_at_symbol(url)]]
+    check = [[having_IP_Address(url),url_length(url),having_at_symbol(url),prefix_suffix(url)]]
 
     return check
+
+# feature 1 -- Using the IP Address
+def having_IP_Address(url):
+    try:
+        ipaddress.ip_address(url)
+        return 1
+    except:
+        return -1
 
 # feature 2 -- Long URL to Hide the Suspicious Part
 def url_length(url):
@@ -26,3 +37,15 @@ def having_at_symbol(url):
         return -1 # No @ symbol
     else:
         return 1
+
+# feature 6 -- Adding Prefix or Suffix Separated by (-) to the Domain
+def prefix_suffix(url):
+    subDomain, domain, suffix = extract(url)
+    # print("subDomain : "+subDomain)
+    # print("Domain : "+domain)
+    # print("Suffix : "+suffix)
+
+    if(domain.count('-')):
+        return 1
+    else:
+        return -1
